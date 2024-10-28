163144341c04dbc0fde1eb533d384533a6195822
nsicart <nsicart.moragas@gmail.com>
2024-10-28 20:01:18 +0100

    Delete pobles/js directory

--- pobles/js/veu.js
+++
@@ -1,6 +0,0 @@
-function reprodueixVeu() {
-    const text = document.getElementById("infoText").textContent;
-    const speech = new SpeechSynthesisUtterance(text);
-    speech.lang = "ca-ES";
-    window.speechSynthesis.speak(speech);
-}
